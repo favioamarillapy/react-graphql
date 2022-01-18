6898118b38c00c4cc433c114c03368539ee96e1c
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-01-18 17:53:18 -0300

    Get courses with apollo client

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,49 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_COURSES = gql`
+  query getCourses {
+    getCourses {
+      _id
+      title
+      teacher
+      description
+      level
+      topic
+      students {
+        _id
+        name
+        email
+        avatar
+      }
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, error, loading } = useQuery(GET_COURSES);
+
+  if (error) return <span className="text-danger">{error}</span>;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React + GraphQL!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+
+        {loading ? (
+          <p>Loading....</p>
+        ) : (
+          <>
+            {data && data.getCourses.map((course) => course.title).join(", ")}
+          </>
+        )}
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </ApolloProvider>,
+  document.getElementById("root")
+);
